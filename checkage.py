0dec316b0dbfb0aaeb6e4633494fa4d13d97b187
CodingPerson691324 <praveen.tapashetti@gmail.com>
2025-04-17 17:18:25 +0100

    initial commit

---
+++ checkage.py
@@ -0,0 +1,31 @@
+print('welcome to class')
+print('please enter your real age')
+age = int(input())
+if age <= 9:
+    print('you are too young')
+elif age == 10:
+    print('you may take the class')
+elif age == 11:
+    print('you may take the class')
+elif age == 12:
+    print('you may take the class')
+elif age == 13:
+    print('you may take the class')
+elif age == 14:
+    print('you may take the class')
+elif age == 15:
+    print('you may take the class')
+elif age == 16:
+    print('you may take the class')
+elif age == 17:
+    print('you may take the class')
+elif age == 18:
+    print('you may take the class')
+elif age == 19:
+    print('you may take the class')
+elif age == 20:
+    print('you may take the class')
+else:
+    print('you are too old')
+
+
