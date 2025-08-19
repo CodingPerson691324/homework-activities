9cf9236ce282dadd07bf45e03fcb23ec662af8bd
CodingPerson691324 <praveen.tapashetti@gmail.com>
2025-08-19 16:53:50 +0100

    intial commit

---
+++ L3-A1.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+window = Tk()
+window.title = ('event handler')
+window.geometry('200x200')
+
+def handle_keypress(event):
+
+    print(event.char)
+
+window.bind('<Key>', handle_keypress)
+
+def handle_click(event):
+    print('\nthe button was clickeed')
+
+button = Button(text='click me!')
+button.pack()
+
+button.bind('<Button-1>', handle_click)
+
+window.mainloop()
